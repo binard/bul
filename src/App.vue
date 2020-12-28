60de2cb1cec677cebd135dee9f3ee811d9418158
binard <jb.bin35@gmail.com>
2020-12-28 22:45:34 +0100

    feat(instagram): add instagram link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <img alt="La BUL - La BUL avant la vie - Since 2017" src="./assets/images/Logo_BUL.png" class="mainlogo" />
+  <div>
+    <img alt="La BUL - La BUL avant la vie - Since 2017" src="./assets/images/Logo_BUL.png" class="mainlogo" />
+    <footer>
+      <a href="https://www.instagram.com/_labul_/" title="instagram la bul" target="_blank"><i class="fab fa-instagram icon"></i></a>
+    </footer>
+  </div>
 </template>
 
 <script>
