d1fa9e899f0975c283003f17c71f940a5425b873
binard <jb.bin35@gmail.com>
2020-12-28 22:57:10 +0100

    feat(donate): add donate link

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div>
     <img alt="La BUL - La BUL avant la vie - Since 2017" src="./assets/images/Logo_BUL.png" class="mainlogo" />
     <footer>
-      <a href="https://www.instagram.com/_labul_/" title="instagram la bul" target="_blank"><i class="fab fa-instagram icon"></i></a>
+      <a href="https://www.instagram.com/_labul_/" title="Instagram la bul" target="_blank"><i class="fab fa-instagram-square icon"></i></a>
+      <a href="https://www.helloasso.com/associations/la-bul/formulaires/1/widget" title="Faire un don" target="_blank"><i class="fas fa-euro-sign icon"></i></a>
     </footer>
   </div>
 </template>
