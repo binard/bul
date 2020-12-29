5b1c2402bd22e84580fb8f9a23e123adfe2addaf
binard <jb.bin35@gmail.com>
2020-12-29 13:41:45 +0100

    feat(status): add status of assocation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <img alt="La BUL - La BUL avant la vie - Since 2017" src="./assets/images/Logo_BUL.png" class="mainlogo" />
-    <footer>
-      <a href="https://www.instagram.com/_labul_/" title="Instagram la bul" target="_blank"><i class="fab fa-instagram-square icon"></i></a>
-      <a href="https://www.helloasso.com/associations/la-bul/formulaires/1/widget" title="Faire un don" target="_blank"><i class="fas fa-euro-sign icon"></i></a>
-    </footer>
-  </div>
+  <div class="main-page">
+      <img alt="La BUL - La BUL avant la vie - Since 2017" src="./assets/images/Logo_BUL.png" class="mainlogo" />
+      <footer>
+        <a href="https://www.instagram.com/_labul_/" title="Instagram la bul" target="_blank"><i class="fab fa-instagram-square icon"></i></a>
+        <a href="https://www.helloasso.com/associations/la-bul/formulaires/1/widget" title="Faire un don" target="_blank"><i class="fas fa-euro-sign icon"></i></a>
+        <a href="/docs/statuts_de_l_association_republicaine_BUL.pdf" title="Télécharger les statuts de l'association" download><i class="fas fa-file-download icon"></i></a>
+      </footer>
+    </div>
 </template>
 
 <script>
